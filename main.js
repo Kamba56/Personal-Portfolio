586d572ff637b4d082f9b0110d94a0462aa3227a
toma <tomajohn316@yahoo.com>
2022-05-10 20:16:05 +0100

    Made mobile menu fucntional

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector('#toggle-menu');
-const icon = document.querySelector('#toggle-menu i')
+const iconBars = document.querySelector('#toggle-menu');
+const iconEx = document.querySelector('#toggle-menu-close')
 const nav = document.querySelector('nav#navbar');
 const body = document.querySelector('body');
 const link = document.querySelectorAll('nav ul li a');
@@ -7,7 +7,27 @@ const navul = document.querySelector('ul.desktop-nav');
 
 function openMenu() {
   nav.classList.replace('d-none', 'd-block');
-  icon.classList.replace('fa-bars', 'fa-xmark');e
+  iconBars.classList.replace('d-block', 'd-none');
+  iconEx.classList.replace('d-none', 'd-block');
+  navul.classList.add('mobile-nav');
+  body.style.position = 'fixed';
+  body.style.top = '-${window.scrollY}px';
 }
 
-menu.addEventListener('click', openMenu);
+function closeMenu() {
+  nav.classList.replace('d-block', 'd-none');
+  iconBars.classList.replace('d-none', 'd-block');
+  iconEx.classList.replace('d-block', 'd-none');
+  navul.classList.remove('mobile-nav');
+  scrollBodyToInitial();
+}
+
+function scrollBodyToInitial() {
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+}
+
+iconBars.addEventListener('click', openMenu);
+iconEx.addEventListener('click', closeMenu);
