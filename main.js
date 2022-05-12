019754ce8a11adccf20585e36277cabb022975db
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-05-12 15:29:51 +0300

    object modal card

--- main.js
+++ main.js
@@ -66,3 +66,78 @@ seeProjectButton.forEach((x) => {
 });
 
 close.addEventListener('click', closeModal);
+
+
+const modalCard = [
+  {
+    id: 1,
+    title: 'Tonic',
+    imageM: './images/PopUpmobile.svg',
+    imageD: './images/PopUpdesktopcopy.svg',
+    heading2: ['Canopy', 'Back End Dev', '2015'],
+    cardDescriptionD:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is    simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea, languages: html, css, javascript",
+    cardDescriptionM:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ",
+    languagesD: ['github', 'ruby', 'Bootstrap'],
+    languagesM: ['html', 'css', 'javascript'],
+    linkLive: 'https://silviatofana.github.io/portfolio/',
+    linkSource: 'https://github.com/silviatofana/portfolio/tree/popup-window',
+  },
+];
+
+const modalCardDisplay = modalCard
+  .map(
+    (modeCard) => `
+
+<div class="card-portfolio-1">
+
+            <li class="pop-close"><i class="fa fa-times"></i></li>
+                <h2 class="tonic-1">${modeCard.title}</h2>
+                    <h5 class="heading-canopy"><b>CANOPY</b> <span>·</span><small> Back End Dev <span>·</span> 2015 </small> </h5>
+         <div class="images">
+         <img class="forD-1" src="${modeCard.imageD}" alt="Pop Up Desktop" >
+      <img class="forM-1" src="${modeCard.imageM}" alt="Pop Up Mobile" >
+
+         </div>    
+      
+      <div>
+      <div class="row">
+      <div class="column-left-1">
+          <p class="forD-1">
+              ${modeCard.cardDescriptionM}
+          </p>
+          <p class="forM-1">
+              ${modeCard.cardDescriptionM}
+          </p>
+
+      </div>
+      <div class="column-right-1">
+          <ul class="modal-1">
+              ${modeCard.languagesM
+    .map((lang) => `<li class="modal-2">${lang}</li>`)
+    .join('')}
+          </ul>
+          <ul id="miss" class="miss modal-1">
+          ${modeCard.languagesD
+    .map((lang) => `<li class="modal-2">${lang}</li>`)
+    .join('')}
+          
+          </ul>
+          <hr>
+          <ul class="modal-1">
+              <li class="modal-3" href="${
+  modeCard.linkLive
+}">See live <img class="btnimag" src="./images/icon/Icon.png"></li>
+              <li class="modal-3" href="${
+  modeCard.linkSource
+}">See Source <i class="fa fa-github"></i></li>
+
+          </ul>
+      </div>
+  </div>
+      </div>
+  </div>
+`,
+  )
+  .join('');
